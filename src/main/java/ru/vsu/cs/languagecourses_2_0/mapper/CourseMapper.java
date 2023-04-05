ea717ab13f4b2cc536aa64be254b5ff2a23b2191
ksmtrv <smotrova-kristina@mail.ru>
2023-04-06 01:43:39 +0300

    Changes by NerdSmith

--- src/main/java/ru/vsu/cs/languagecourses_2_0/mapper/CourseMapper.java
+++ src/main/java/ru/vsu/cs/languagecourses_2_0/mapper/CourseMapper.java
@@ -9,11 +9,17 @@ public class CourseMapper implements Mapper<Course, CourseDto>{
 
     @Override
     public CourseDto toDTO(Course entity) {
-        return null;
+        CourseDto courseDto = new CourseDto();
+        courseDto.setPrice(entity.getPrice());
+        courseDto.setTitle(entity.getTitle());
+        return courseDto;
     }
 
     @Override
     public Course toEntity(CourseDto dto) {
-        return null;
+        Course course = new Course();
+        course.setTitle(dto.getTitle());
+        course.setPrice(dto.getPrice());
+        return course;
     }
 }
--- src/main/java/ru/vsu/cs/languagecourses_2_0/service/CourseService.java
+++ src/main/java/ru/vsu/cs/languagecourses_2_0/service/CourseService.java
@@ -1,11 +1,19 @@
 package ru.vsu.cs.languagecourses_2_0.service;
 
 import org.springframework.stereotype.Service;
+import ru.vsu.cs.languagecourses_2_0.entity.Course;
+import ru.vsu.cs.languagecourses_2_0.entity.Level;
+import ru.vsu.cs.languagecourses_2_0.entity.dto.CourseDto;
+import ru.vsu.cs.languagecourses_2_0.entity.dto.CourseFullDto;
+import ru.vsu.cs.languagecourses_2_0.mapper.CourseFullMapper;
 import ru.vsu.cs.languagecourses_2_0.mapper.CourseMapper;
 import ru.vsu.cs.languagecourses_2_0.repository.CourseRepository;
 import ru.vsu.cs.languagecourses_2_0.repository.IntensityRepository;
 import ru.vsu.cs.languagecourses_2_0.repository.LanguageRepository;
 import ru.vsu.cs.languagecourses_2_0.repository.LevelRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CourseService {
